d52265c9449074eb961f1de476065434595f086e
Gizot <tastangizem@gmail.com>
2023-07-14 23:30:48 +0300

    commit repeat

--- src/assets/defaultusers.ts
+++ src/assets/defaultusers.ts
@@ -1,70 +1,70 @@
 export const defaultusers = [
     {
       userId: 1,
-      username: "pspera0",
+      username: "tsunade",
       email: "hcasseldine0@sitemeter.com",
       creationDate: "2018-08-17",
       isActive: false
     },
     {
       userId: 2,
-      username: "ttiddy1",
+      username: "tobby1",
       email: "echapelle1@friendfeed.com",
       creationDate: "2018-03-01",
       isActive: true
     },
     {
       userId: 3,
-      username: "tbankes2",
+      username: "erosennin",
       email: "fwelbeck2@berkeley.edu",
       creationDate: "2019-12-31",
       isActive: true
     },
     {
       userId: 4,
-      username: "shardway3",
+      username: "beesama",
       email: "jberzen3@unblog.fr",
       creationDate: "2018-09-04",
       isActive: false
     },
     {
       userId: 5,
-      username: "mgaffon4",
+      username: "connan",
       email: "dbartram4@networkadvertising.org",
       creationDate: "2017-04-17",
       isActive: false
     },
     {
       userId: 6,
-      username: "tniccols5",
+      username: "itachi",
       email: "dellerker5@hugedomains.com",
       creationDate: "2019-11-10",
       isActive: false
     },
     {
       userId: 7,
-      username: "hgori6",
+      username: "kisame",
       email: "dclavering6@imdb.com",
       creationDate: "2019-02-16",
       isActive: true
     },
     {
       userId: 8,
-      username: "lkiebes7",
+      username: "kakashi",
       email: "kspaight7@cisco.com",
       creationDate: "2015-08-09",
       isActive: false
     },
     {
       userId: 9,
-      username: "bcairney8",
+      username: "katsu",
       email: "ncattrall8@networksolutions.com",
       creationDate: "2015-11-19",
       isActive: true
     },
     {
       userId: 10,
-      username: "vslator9",
+      username: "sasori",
       email: "jbrambley9@bloglovin.com",
       creationDate: "2020-03-07",
       isActive: true
